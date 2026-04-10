885081337c5cea8f959290c3f15579d80d32b82e
Priyanshu Gupta <priyanshug.cs.25@nitj.ac.in>
2026-04-11 00:14:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,16 @@ const revisionRoutes  = require('./routes/revision');
 const app  = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://leetcodetrackerpriyanshucoder.netlify.app",
+    "http://localhost:3000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
